cd362e7b4a756be387cd140a4aa65cdb90600887
bharanisrivatsa <3010.bharanisrivatsa@gmail.com>
2026-04-15 12:15:25 +0530

    modified files

--- tests/Letcode.first.spec.js
+++ tests/Letcode.first.spec.js
@@ -1,8 +1,9 @@
-import {test,expect} from '@playwright/test';
+import {test,expect} from '../pages/fixtures';  
 
-test('example page title', async ({ page }) => {
-  await page.goto('https://letcode.in/');
-  await expect(page).toHaveTitle( "LetCode with Koushik");
-  await page.getByRole('link',{name: 'Work-Space'}).click();
-  await page.getByRole('link',{name: ' Simple table'}).click();
+
+
+
+test('example page title', async ({ WorkSpacePage }) => {
+    await expect(WorkSpacePage).toHaveTitle( "LetCode with Koushik");
+  
 });
